5c2cb610c1edd744dc150f32765d5d40982392b3
N1hron <denik8008@gmail.com>
2023-10-15 18:26:52 +0300

    create root variable

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
+import './firebase'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { Provider } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
-import './firebase'
 
 import App from './components/App'
 import './styles/normalize.css'
@@ -11,7 +11,9 @@ import GlobalStyle from './styles/GlobalStyle'
 import { themes } from './styles/themes'
 
 
-createRoot(document.getElementById('root')).render(
+const root = createRoot(document.getElementById('root'))
+
+root.render(
     <StrictMode>
         <Provider store={ store }>
             <ThemeProvider theme={ themes.main }>
